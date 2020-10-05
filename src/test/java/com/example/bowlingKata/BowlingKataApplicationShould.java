c84bda18b15ed5dbc73fa08815b02649ed0aa3d7
Carles Torrat <ctorrat@idealista.com>
2020-10-05 14:15:45 +0200

    Tests naming

--- src/test/java/com/example/bowlingKata/BowlingKataApplicationShould.java
+++ src/test/java/com/example/bowlingKata/BowlingKataApplicationShould.java
@@ -10,23 +10,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-class BowlingKataApplicationTests {
+class BowlingKataApplicationShould {
 	@Test
-	public void should_score_90_points_when_9_pins_in_each_turn() throws NotImplementedException {
+	public void score_90_points_when_9_pins_in_each_turn() throws NotImplementedException {
 		GameResult gameResult = GameResultStub.noStrikesAndNoSparesWithNumberOfKnockedPins(9);
 		BowlingPrinter.print(gameResult);
 		assertEquals(90, gameResult.score());
 	}
 
 	@Test
-	public void should_score_150_points_when_10_pairs_of_5_and_spare_with_final_5() throws NotImplementedException {
+	public void score_150_points_when_10_pairs_of_5_and_spare_with_final_5() throws NotImplementedException {
 		GameResult gameResult = GameResultStub.allSparesWithNumberOfKnokedPins(5);
 		BowlingPrinter.print(gameResult);
 		assertEquals(150, gameResult.score());
 	}
 
 	@Test
-	public void should_score_300_points_when_all_strikes() throws NotImplementedException {
+	public void score_300_points_when_all_strikes() throws NotImplementedException {
 		GameResult gameResult = GameResultStub.allStrikes();
 		BowlingPrinter.print(gameResult);
 		assertEquals(300, gameResult.score());
